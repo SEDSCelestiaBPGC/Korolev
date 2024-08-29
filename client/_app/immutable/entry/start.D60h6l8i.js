6274f1eeb8b4c313d94b162bbcb38a2e8d5db657
git-pi-e <git-pi-e@users.noreply.github.com>
2024-08-29 21:31:45 +0000

    Deploying to gh-pages from @ SEDSCelestiaBPGC/Korolev@c6d10d65c497b2d217aa3859032e125f25db3a26 🚀

---
+++ client/_app/immutable/entry/start.D60h6l8i.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CvVz5-qW.js";export{t as start};
---
+++ server/nodes/5.js
@@ -0,0 +1,8 @@
+
+
+export const index = 5;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/pages/projects/_page.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/5.BMLrrv89.js","_app/immutable/chunks/scheduler.DzV7pw1l.js","_app/immutable/chunks/index.OuQUVrRW.js","_app/immutable/chunks/each.D6YF6ztN.js","_app/immutable/chunks/canvas.BK11fjbc.js","_app/immutable/chunks/image.Caopvt9C.js","_app/immutable/chunks/slider.svelte_svelte_type_style_lang.BCrG7bZt.js","_app/immutable/chunks/paths.CWSbX0dy.js"];
+export const stylesheets = ["_app/immutable/assets/5.Y-ChjlVd.css","_app/immutable/assets/slider.BcBvyb-Y.css"];
+export const fonts = [];
